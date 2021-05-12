718b0a9fca6138fc33efe00228ad7d05a7e9528e
Clayton Cook <claytoncook12@gmail.com>
2021-05-12 09:35:09 -0400

    Add TuneType to admin panel

--- autosession/admin.py
+++ autosession/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tune, Key, Instrument, Note, Recording
+from .models import Tune, Key, Instrument, Note, Recording, TuneType
 
 # Register your models here.
 admin.site.register(Tune)
@@ -7,3 +7,4 @@ admin.site.register(Key)
 admin.site.register(Instrument)
 admin.site.register(Note)
 admin.site.register(Recording)
+admin.site.register(TuneType)
