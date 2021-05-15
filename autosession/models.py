eb3f6d4865c7bf251d5299898c14cf561a708629
Clayton Cook <claytoncook12@gmail.com>
2021-05-15 10:11:45 -0400

    Move tune_type to Tune table

--- autosession/models.py
+++ autosession/models.py
@@ -2,10 +2,18 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class TuneType(models.Model):
+    tune_type_id = models.AutoField(primary_key=True)
+    tune_type_char = models.CharField('Tune Type', max_length=50, unique=True)
+
+    def __str__(self):
+        return self.tune_type_char
+
 class Tune(models.Model):
     tune_id = models.AutoField(primary_key=True)
     name = models.CharField('Tune Name', max_length=300)
     parts = models.IntegerField('Number of Parts in Tune')
+    tune_type = models.ForeignKey(TuneType, on_delete=models.CASCADE, verbose_name="Tune Type", null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -16,13 +24,6 @@ class Key(models.Model):
 
     def __str__(self):
         return f'{self.key_type_char}'
-
-class TuneType(models.Model):
-    tune_type_id = models.AutoField(primary_key=True)
-    tune_type_char = models.CharField('Tune Type', max_length=50, unique=True)
-
-    def __str__(self):
-        return f'{self.tune_type_char}'
 
 class Instrument(models.Model):
     instrument_id = models.AutoField(primary_key=True)
@@ -52,7 +53,6 @@ class Recording(models.Model):
     recording_url = models.URLField('Recording URL', null=True, blank=True)
     date_recorded = models.DateField(null=True, blank=True)
     tune = models.ForeignKey(Tune, on_delete=models.CASCADE, verbose_name="Tune", null=True, blank=True)
-    tune_type = models.ForeignKey(TuneType, on_delete=models.CASCADE, verbose_name="Tune Type", null=True, blank=True)
     key = models.ForeignKey(Key, on_delete=models.CASCADE, verbose_name="Key of Tune", null=True, blank=True)
     insturment = models.ForeignKey(Instrument, on_delete=models.CASCADE, verbose_name="Instrument used during recording", null=True, blank=True)
     bpm = models.IntegerField('Beats Per Minute of Recording')
--- autosession/serializers.py
+++ autosession/serializers.py
@@ -5,4 +5,4 @@ from .models import Tune
 class TuneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tune
-        fields = ('tune_id','name', 'parts')
+        fields = ('tune_id','name', 'parts','tune_type')
