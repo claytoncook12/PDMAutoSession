6fb5e9ea9be1e0e411b4e7a8d0d3883effb2efff
Clayton Cook <claytoncook12@gmail.com>
2021-05-19 14:30:24 -0400

    updated installed_apps list

--- core/settings.py
+++ core/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'autosession',
     'rest_framework',
+    'autosession',
 ]
 
 MIDDLEWARE = [
